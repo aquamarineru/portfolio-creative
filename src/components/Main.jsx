abfa05e2ff08cc421cbf949209498aa604365ca9
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-02-08 20:16:37 +0100

    dark step1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,14 +13,14 @@ import { FaTwitter, FaInstagram, FaLinkedinIn, FaTelegram } from 'react-icons/fa
 export default function Main() {
   return (
     <div id='main'>
-        <div className='w-full h-screen flex flex-col justify-center items-center absolute top-0 left-0 bg-[#E8EDDF] lg:items-end'>
-            <button className='mt-10 border border-black rounded-lg py-[10px] px-[20px] cursor-pointer shadow-lg hover:bg-[#F5CB5C] scale-110 easy-in duration-200 lg:mr-10'>
-                <a href="#" className='font-["Inconsolata"] text-lg lg:text-2xl'>Work with me</a>
+        <div className='w-full h-screen flex flex-col justify-center items-center absolute top-0 left-0 bg-[#E8EDDF] lg:items-end dark:bg-[#0D060F]'>
+            <button className='mt-10 border border-black rounded-lg py-[10px] px-[20px] cursor-pointer shadow-lg hover:bg-[#F5CB5C] scale-110 easy-in duration-200 lg:mr-10 dark:border-white/80 hover:dark:bg-[#FCA311]'>
+                <a href="#" className='font-["Inconsolata"] text-lg lg:text-2xl dark:text-white/80'>Work with me</a>
             </button>
-            <div className='container m-auto px-4 max-w-[300px] h-full w-full flex flex-col justify-end items-center mb-8 bg-[url("src/img/main-light.png")] bg-no-repeat bg-top bg-contain bg-[length:320px_300px] md:max-w-[500px] lg:max-w-[900px] lg:bg-[length:420px_400px] lg:bg-right xl:max-w-[1000px]'>
+            <div className='container m-auto px-4 max-w-[320px] h-full w-full flex flex-col justify-end items-center bg-[url("src/img/main-light.png")] bg-no-repeat bg-top bg-contain bg-[length:320px] md:max-w-[500px] lg:max-w-[900px] lg:bg-[length:420px] lg:bg-right xl:max-w-[1200px] xl:bg-[length:530px] dark:bg-[url("src/img/main-dark.png")] dark'>
                 <div className='m-0 absolute top-[60%] left-[15%] md:left-[30%] lg:left-[15%] lg:top-[40%]'>
-                    <div className='absolute top-[-20px] left-[-20px] w-[70px] h-[70px] bg-[#CFDBD5] rounded-lg absolute z-0 lg:w-[100px] lg:h-[100px]'></div>
-                    <div className='absolute min-w-max py-4 px-4 pr-4 bg-white border border-black rounded-lg shadow-lg shadow-gray-400 z-10 lg:px-6 lg:py-6'>
+                    <div className='absolute top-[-20px] left-[-20px] w-[70px] h-[70px] bg-[#CFDBD5] shadow-lg shadow-gray-600 rounded-lg absolute z-0 lg:w-[100px] lg:h-[100px] dark:bg-[#3D405B]'></div>
+                    <div className='absolute min-w-max py-4 px-4 pr-4 bg-white border border-black rounded-lg shadow-lg shadow-gray-500 z-10 lg:px-6 lg:py-6 dark:bg-[#E5E5E5] dark:border-[#E5E5E5]'>
                     <h1 className='text-lg font-["Inconsolata"] font-bold text-gray-800 lg:text-2xl'>01  Hi! I'm Marina Ilyushina </h1>
                     <h2 className='flex text-lg font-["Inconsolata"] font-bold text-gray-800 lg:text-2xl'>02 I'm a 
                     <TypeAnimation
@@ -42,24 +42,24 @@ export default function Main() {
                 </div>
             </div>
             <div className='flex justify-between max-w-[200px] w-full lg:absolute lg:left-[20%] lg:top-[60%]'>
-                    <FaTelegram size={30} className='cursor-pointer text-gray-600' />
-                    <FaTwitter size={30} className='cursor-pointer text-gray-600' />
-                    <FaInstagram size={30} className='cursor-pointer text-gray-600' />
-                    <FaLinkedinIn size={30} className='cursor-pointer text-gray-600' />
+                    <FaTelegram size={30} className='cursor-pointer text-gray-600 dark:text-white/80' />
+                    <FaTwitter size={30} className='cursor-pointer text-gray-600 dark:text-white/80' />
+                    <FaInstagram size={30} className='cursor-pointer text-gray-600 dark:text-white/80' />
+                    <FaLinkedinIn size={30} className='cursor-pointer text-gray-600 dark:text-white/80' />
             </div>
         <div className='invisible md:visible'>
-            <FiFigma size={50} className='absolute text-gray-500 top-[15%] left-[65%] hover:animate-wiggle lg:left-[45%] lg:top-[25%]'/>
-            <ImHtmlFive2 size={50} className='absolute text-gray-500 top-[15%] left-[25%] hover:animate-wiggle lg:left-[30%]'/>
-            <SiCsswizardry size={50} className='absolute text-gray-500 top-[30%] left-[15%] hover:animate-wiggle lg:top-[25%]'/>
-            <SiTailwindcss size={50} className='absolute text-gray-500 top-[30%] left-[75%] hover:animate-wiggle lg:left-[45%] lg:top-[70%]'/>
-            <IoLogoJavascript size={50} className='absolute text-gray-500 top-[50%] left-[15%] hover:animate-wiggle'/>
-            <GrReactjs size={50} className='absolute text-gray-500 top-[60%] left-[75%] hover:animate-spin lg:left-[30%] lg:top-[80%]'/>
-            <SiTypescript size={50} className='absolute text-gray-500 top-[45%] left-[80%] hover:animate-wiggle lg:left-[15%] lg:top-[70%]'/>
+            <FiFigma size={50} className='absolute text-gray-500 top-[15%] left-[65%] hover:animate-wiggle lg:left-[45%] lg:top-[25%] dark:text-white/80'/>
+            <ImHtmlFive2 size={50} className='absolute text-gray-500 top-[15%] left-[25%] hover:animate-wiggle lg:left-[30%] dark:text-white/80'/>
+            <SiCsswizardry size={50} className='absolute text-gray-500 top-[30%] left-[15%] hover:animate-wiggle lg:top-[25%] dark:text-white/80'/>
+            <SiTailwindcss size={50} className='absolute text-gray-500 top-[30%] left-[75%] hover:animate-wiggle lg:left-[45%] lg:top-[70%] dark:text-white/80'/>
+            <IoLogoJavascript size={50} className='absolute text-gray-500 top-[50%] left-[15%] hover:animate-wiggle lg:left-[30%] lg:top-[30%] xl:left-[50%] xl:top-[45%] dark:text-white/80'/>
+            <GrReactjs size={50} className='absolute text-gray-500 top-[60%] left-[75%] hover:animate-spin lg:left-[30%] lg:top-[80%] dark:text-white/80'/>
+            <SiTypescript size={50} className='absolute text-gray-500 top-[45%] left-[80%] hover:animate-wiggle lg:left-[15%] lg:top-[70%] dark:text-white/80'/>
             </div>
             <div className='max-w-[200px] w-full flex justify-between items-center mt-8 lg:max-w-[250px]'>
-                <button className='rounded-lg py-[10px] px-[20px] cursor-pointer bg-[#F5CB5C] shadow-lg hover:border border-black scale-110 easy-in duration-200 font-["Inconsolata"] text-lg lg:text-2xl'>Read more</button>
-                <button className='animate-bounce rounded-full py-[1rem] px-[1rem] cursor-pointer bg-[#F5CB5C] shadow-lg hover:border border-black scale-110 easy-in duration-200'>
-                    <AiOutlineArrowDown size={20}/>
+                <button className='rounded-lg py-[10px] px-[20px] cursor-pointer bg-[#F5CB5C] shadow-lg hover:border border-black scale-110 easy-in duration-200 font-["Inconsolata"] text-lg lg:text-2xl dark:bg-[#FCA311] dark:text-white/80 hover:dark:border-white/80'>Read more</button>
+                <button className='animate-bounce rounded-full py-[1rem] px-[1rem] cursor-pointer bg-[#F5CB5C] shadow-lg hover:border border-black scale-110 easy-in duration-200 dark:bg-[#FCA311] hover:dark:border-white/80 '>
+                    <AiOutlineArrowDown size={20} className='dark:text-white/80'/>
                 </button>
             </div>
         </div>
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -16,40 +16,40 @@ export default function SideNav() {
   return (
     <div>
       <AiOutlineMenu 
-        className='absolute top-4 right-4 z-[99] md:hidden'
+        className='absolute top-4 right-4 z-[99] md:hidden dark:text-white'
         onClick={handleNav}
         />
         {
           nav ? (
             <>
             <div 
-              className='fixed w-full h-screen bg-[#E8EDDF] flex flex-col justify-center items-center z-20'>
+              className='fixed w-full h-screen bg-[#E8EDDF] flex flex-col justify-center items-center z-20 dark:bg-[#0D060F]'>
               <a href='#main' 
-                className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                 <AiOutlineHome size={20}/>
-                <span className='pl-4'>Home</span>
+                <span className='pl-4 dark:text-white/80'>Home</span>
               </a>
               <a href='#main' 
                 className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5]
-                 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                 <FaUniversity size={20}/>
-                <span className='pl-4'>Education</span>
+                <span className='pl-4 dark:text-white/80'>Education</span>
               </a>
               <a href='#main' 
                 className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5]
-                 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
-                <GrProjects size={20}/>
-                <span className='pl-4'>Projects</span>
+                 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
+                <GrProjects size={20} />
+                <span className='pl-4 dark:text-white/80'>Projects</span>
               </a>
               <a href='#main' 
-                className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                 <BsPerson size={20}/>
-                <span className='pl-4'>Resume</span>
+                <span className='pl-4 dark:text-white/80'>Resume</span>
               </a>
               <a href='#main' 
-                className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                className='w-[75%] flex justify-center rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                 <AiOutlineMail size={20}/>
-                <span className='pl-4'>Contact</span>
+                <span className='pl-4 dark:text-white/80'>Contact</span>
               </a>
             </div>
             </>
@@ -61,24 +61,24 @@ export default function SideNav() {
               <div className='flex flex-col'>
                 <a href="#main" 
                   className='rounded-full shadow-lg bg-[#CFDBD5]
-                   shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                   shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                   <AiOutlineHome size={20}/>
                 </a>
                 <a href="#main" 
                   className='rounded-full shadow-lg bg-[#CFDBD5]
-                   shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                   shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                   <FaUniversity size={20}/>
                 </a>
                 <a href="#main" 
-                  className='rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                  className='rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#FCA311] dark:text-white/80'>
                   <GrProjects size={20}/>
                 </a>
                 <a href="#main" 
-                  className='rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                  className='rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#3D405B] dark:text-white/80'>
                   <BsPerson size={20}/>
                 </a>
                 <a href="#main" 
-                  className='rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200'>
+                  className='rounded-full shadow-lg bg-[#CFDBD5] shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 easy-in duration-200 dark:bg-[#3D405B] dark:text-white/80'>
                   <AiOutlineMail size={20} className='color-white'/>
                 </a>
               </div>
